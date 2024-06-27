64cb9374d6806bccb26b1614142368aba9e55390
Nada Tharwat <106287661+nada15987@users.noreply.github.com>
2024-06-27 22:55:06 +0300

    Add files via upload

---
+++ 01_introduction/S1_task2.py
@@ -0,0 +1,7 @@
+#task2
+#Write a Python program which accepts the radius of a circle from the user and compute the area.
+print('please, enter the radius of a circle')
+r= float(input())
+pi=3.14159
+area = pi*(r**2)
+print ("the area of the circle of radius " ,r ," is :", area) 
